2971b4d0e6450057c717b3677ac6921d59710961
AndreLuSantana <andrelusantana@hotmail.com>
2024-11-28 14:10:00 -0300

    Implementação padrão DTO e criação da classe TecnicoDTO

--- src/main/java/com/andredev/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/andredev/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andredev.helpdesk.domain.Tecnico;
+import com.andredev.helpdesk.domain.DTO.TecnicoDTO;
 import com.andredev.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,9 +19,9 @@ public class TecnicoResource {
 	private TecnicoService service;
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById (@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById (@PathVariable Integer id){
 		Tecnico obj = this.service.findByid(id);
 		
-		return ResponseEntity.ok().body(obj);	
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));	
 	}
 }

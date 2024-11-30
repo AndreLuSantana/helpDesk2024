b26f98204e69ec96428dd6cadbc9b268daf7edad
AndreLuSantana <andrelusantana@hotmail.com>
2024-11-30 10:56:05 -0300

    Criação metodos getById e getByEmail

--- src/main/java/com/andredev/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/andredev/helpdesk/repositories/PessoaRepository.java
@@ -3,8 +3,14 @@ package com.andredev.helpdesk.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.andredev.helpdesk.domain.Pessoa;
+import java.util.List;
+import java.util.Optional;
+
+
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{ //classe reponsável de relizar a persistencia no DB
 
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 	
 }

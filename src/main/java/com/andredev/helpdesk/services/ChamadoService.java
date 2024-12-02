68f2081d11611b5a0da7a6d24c395748cb3b0c31
AndreLuSantana <andrelusantana@hotmail.com>
2024-12-02 09:56:56 -0300

    Implementacao findAll para chamado

--- src/main/java/com/andredev/helpdesk/services/ChamadoService.java
+++ src/main/java/com/andredev/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.andredev.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class ChamadoService {
 		Optional<Chamado> obj = chamadoRepository.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado " + id));
+	}
+
+	public List<Chamado> findAll() {
+		return chamadoRepository.findAll();
 	}
 }
